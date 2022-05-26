6a49ed3983edb0d2a20bcd41a5e03cb7b726ba0b
hayabusapb <95869683+hayabusapb@users.noreply.github.com>
2022-05-26 18:23:30 -0400

    Delete call_data.sh

--- call_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:30:00
-#SBATCH --account=def-adagher
-#SBATCH --mem-per-cpu 18G
-
-module load singularity/3.8
-singularity exec --overlay /lustre03/project/6008063/neurohub/ukbb/imaging/.tractoflow/neurohub_ukbb_tractoflow_1.squashfs:ro --bind /home/hayabusa/scratch/:/OUT ~/wma_done/SlicerJN27.sif ./fetch_data.sh
-
