51b0655c55a5df6b8f0f25781ee93838982c8e51
hayabusapb <95869683+hayabusapb@users.noreply.github.com>
2022-05-26 18:37:01 -0400

    Update pyradio_Simple.sh

--- pyradio_Simple.sh
+++ pyradio_Simple.sh
@@ -10,8 +10,6 @@ cd $IN
 for f in *
 do
  echo "Processing $f - reference is $REF - saving $OUT/$f"
- #python3 ~/wm_t2vol.py $IN/$f $REF $OUT/${f%.*}
- 
  pyradiomics $REF $IN/$f -o $OUT -f 'csv' --format-path 'basename' --setting 'geometryTolerance: 0.00001'
 done
 
